8e1d6521210f7f1f053283e7eb4711b2de40b210
Dahee Goo <heeouo@kookmin.ac.kr>
2021-08-16 21:03:20 +0900

    chore: 게시글 목록 조회 - 자잘한 코드 수정

--- src/main/java/com/book/springboot/web/IndexController.java
+++ src/main/java/com/book/springboot/web/IndexController.java
@@ -26,5 +26,4 @@ public class IndexController {
         return "posts-save";
     }
 
-
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -28,7 +28,7 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 등록되었습니다.');
-            window.location.href = '/'; // 등록 성공 시 메인 페이지로 이동
+            window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
@@ -73,10 +73,3 @@ var main = {
 };
 
 main.init();
-
-/*
-브라우저의 스코프는 공용 공간이므로 중복된 함수 이름이 자주 발생할 수 있음
--> js 단일 파일만의 유효범위를 만들어 사용하자!!
--> 객체를 만들고, 해당 객체 "내부 속성"으로 필요한 함수들을 추가하기
--> 객체 안에서만 유효하므로 다른 js 파일들과 겹칠 위험 x
- */
