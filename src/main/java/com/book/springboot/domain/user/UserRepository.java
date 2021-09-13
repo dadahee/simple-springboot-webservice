9a9cad8ab73e8ab37884dcf94e763d52d4406abd
Dahee Goo <heeouo@kookmin.ac.kr>
2021-09-13 14:25:15 +0900

    chore: UserRepository 오타 수정

--- src/main/java/com/book/springboot/domain/user/UserRepository.java
+++ src/main/java/com/book/springboot/domain/user/UserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public class UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     // 소셜 로그인으로 이미 생성된 사용자 이메일인지, 처음 가입하는 사용자 이메일인지 판단
     Optional<User> findByEmail(String email);
 }
