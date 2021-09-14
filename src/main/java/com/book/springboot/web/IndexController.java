fbc80c67f59a47d4063b7a15e1b5e5ab13e7e11a
Dahee Goo <heeouo@kookmin.ac.kr>
2021-09-14 21:40:01 +0900

    chore: IndexController 및 index.mustache 파일에 계정 연결 추가 - 로그인 시 new post 노출 - 로그인 / 로그아웃 버튼 노출 - 로그인한 유저네임 노출

--- src/main/java/com/book/springboot/web/IndexController.java
+++ src/main/java/com/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.book.springboot.web;
 
+import com.book.springboot.config.auth.dto.SessionUser;
+import com.book.springboot.domain.user.User;
 import com.book.springboot.service.posts.PostsService;
 import com.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,17 +9,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) model.addAttribute("userName", user.getName());
         // 머스테치 스타터 덕분에
         // 컨트롤러에서 문자열을 반환할 때
         // 앞의 디렉토리 경로와 뒤의 파일 확장자는 자동으로 지정
