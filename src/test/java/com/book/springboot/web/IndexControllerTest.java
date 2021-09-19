6d21d88c521364fc932d61b8f4e212d70c936806
Dahee Goo <heeouo@kookmin.ac.kr>
2021-09-19 11:24:05 +0900

    fix: IndexControllerTest 오류 수정

--- src/test/java/com/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/book/springboot/web/IndexControllerTest.java
@@ -28,7 +28,7 @@ class IndexControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         // then
-        assertThat(body).contains("스프링 부트로 시작하는 웹서비스");
+        assertThat(body).contains("Simple Springboot Web Service");
     }
 
 }
